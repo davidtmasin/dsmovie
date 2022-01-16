04b7c1c946676fcad12ae74ee875e9d48cd43aa5
David Teixeira de Masin <davidteixeira.info@gmail.com>
2022-01-16 20:46:29 -0300

    Movie Card

---
+++ frontend/src/components/MovieCard/index.tsx
@@ -0,0 +1,27 @@
+import MovieScore from 'components/MovieScore'
+
+function MovieCard() {
+  const movie = {
+    id: 1,
+    image: 'https://pbs.twimg.com/media/DkRxmnBW4AEztYG.jpg',
+    title: 'Ursos Grandes, Peludos e Mansos',
+    count: 2,
+    score: 4.5
+  }
+  return (
+    <div>
+      <img
+        className="dsmovie-movie-card-image"
+        src={movie.image}
+        alt={movie.title}
+      />
+      <div className="dsmovie-card-bottom-container">
+        <h3>{movie.title}</h3>
+        <MovieScore />
+        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+      </div>
+    </div>
+  )
+}
+
+export default MovieCard
