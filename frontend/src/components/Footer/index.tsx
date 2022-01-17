9f349f6443272e155f9d63dd042aca9a325e7b8e
David Teixeira de Masin <davidteixeira.info@gmail.com>
2022-01-17 10:45:45 -0300

    Footer Component JSX+CSS

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+import './styles.css'
+
+function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="dsmovie-footer-content">
+          <p>
+            Projeto desenvolvido por <strong>David Teixeira</strong> durante a
+            Semana Spring React.
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
